47224cc80bac16e5df927f174b3496774df318b4
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2024-10-04 15:57:55 -0400

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Gerrit Van Leeuwen on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
