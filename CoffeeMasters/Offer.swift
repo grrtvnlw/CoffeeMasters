c894a10e741dec6667f4877921c1add541536b61
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2024-12-12 10:52:18 -0500

    adding Offer View

---
+++ CoffeeMasters/Offer.swift
@@ -0,0 +1,20 @@
+//
+//  Offer.swift
+//  CoffeeMasters
+//
+//  Created by Gerrit Van Leeuwen on 12/12/24.
+//
+
+import SwiftUI
+
+struct Offer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Offer_Previews: PreviewProvider {
+    static var previews: some View {
+        Offer()
+    }
+}
